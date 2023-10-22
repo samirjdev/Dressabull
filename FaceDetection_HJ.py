b7b7ec2922fd3b8f1e5bfad3afc10b2be84fc7ce
TechStar1166 <86173942+TechStar1166@users.noreply.github.com>
2023-10-22 12:50:54 -0400

    Update FaceDetection_HJ.py

--- FaceDetection_HJ.py
+++ FaceDetection_HJ.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Load an image
-image = cv2.imread('Test_Image2.jpg')
+image = cv2.imread('Test_Image1.jpeg')
 
 # Convert the image to grayscale for better detection
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -13,25 +13,32 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
 # Create a folder to save the cropped faces (if it doesn't exist)
-import os
-if not os.path.exists('cropped_faces'):
-    os.mkdir('cropped_faces')
+#import os
+#if not os.path.exists('cropped_faces'):
+    #os.mkdir('cropped_faces')
 
-# Counter for naming the cropped face images
-face_count = 0
+# Initialize variables to keep track of the largest face
+largest_area = 0
+largest_face = None
 
-# Process only the first detected face (if any)
-if len(faces) > 0:
-    x, y, w, h = faces[0]  # Get the coordinates of the first detected face
+# Iterate through the detected faces to find the largest one
+for (x, y, w, h) in faces:
     face = image[y:y + h, x:x + w]
-    cv2.imwrite(f'cropped_faces/face_{face_count}.jpg', face)
+    face_area = w * h
     
-    # Draw a rectangle around the first detected face
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-    face_count += 1
+    if face_area > largest_area:
+        largest_area = face_area
+        largest_face = face
 
-# Display the image with the detected face(s)
-cv2.imshow('Face Detection', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Save the largest face
+if largest_face is not None:
+    cv2.imwrite('largest_face.jpg', largest_face)
+    
+    # Draw a rectangle around the largest detected face
+    x, y, w, h = faces[0]
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+# Display the image with the largest detected face
+#cv2.imshow('Face Detection', image)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
